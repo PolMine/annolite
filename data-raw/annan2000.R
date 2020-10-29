10cec591984a9e97801bfa2c0ef54c6e9f4ba4c9
Andreas Blaette <andreas.blaette@uni-due.de>
2020-10-29 14:55:12 +0100

    annan sample data updated

--- data-raw/annan2000.R
+++ data-raw/annan2000.R
@@ -1,16 +1,31 @@
 library(polmineR)
 library(annolite)
 
-ftab <- corpus("UNGA") %>%
+secretary_general_2000 <- corpus("UNGA") %>%
   subset(date == "2000-04-03") %>%
   subset(speaker == "The Secretary-General") %>% 
   fulltexttable()
-ftab[["tokenstream"]] <- lapply(ftab[["tokenstream"]], function(x)x[c(-1, -nrow(x)),])
 
-secretary_general_2000 <- ftab
+secretary_general_2000[["tokenstream"]] <- lapply(
+  secretary_general_2000[["tokenstream"]],
+  function(x) x[c(-1, -nrow(x)),]
+)
 
 save(
   secretary_general_2000,
   file = "~/Lab/github/annolite/data/sg.RData",
   compress = "xz"
 )
+
+if (interactive()){
+  annotate(
+    secretary_general_2000,
+    buttons = list(
+      organisation = "yellow",
+      document = "lightgreen",
+      date = "lightblue",
+      issue = "lightgreen",
+      geolocation = "red"
+    )
+  )
+}
